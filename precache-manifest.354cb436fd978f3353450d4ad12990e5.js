b8de0892fb74d69105dffa27d75156fbd7ddbeb4
unknown <daliborpetric99@gmail.com>
2019-05-14 16:59:30 +0200

    Updates

---
+++ precache-manifest.354cb436fd978f3353450d4ad12990e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca56b1593f4b0f26e7bfb6c1295a366",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "4e76a3be1fd6f6444335",
+    "url": "/react-movies/static/css/main.8bf8dac8.chunk.css"
+  },
+  {
+    "revision": "4145c5cf55f83a4e5736",
+    "url": "/react-movies/static/js/2.b6846033.chunk.js"
+  },
+  {
+    "revision": "4e76a3be1fd6f6444335",
+    "url": "/react-movies/static/js/main.ff20fd53.chunk.js"
+  },
+  {
+    "revision": "cdb8474b72bd7dff1c63",
+    "url": "/react-movies/static/js/runtime~main.bf100f6d.js"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/react-movies/static/media/header_logo.08494abf.png"
+  },
+  {
+    "revision": "40f8a3bb0576fb9e296fa4ea4253446d",
+    "url": "/react-movies/static/media/poster.40f8a3bb.jpg"
+  }
+]);
